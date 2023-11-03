b0f7aaa026853640e2fe728826aea4aac264fc4d
Hazal Taştan <hazl.dogann@gmail.com>
2023-11-03 13:57:18 +0300

    data prepared

---
+++ web-sayfam/src/api/data.js
@@ -0,0 +1,94 @@
+export const headerData = [
+  {
+    paragraph: {
+      en: "I am a Frontend Developer...",
+      tr: "Ben Frontend Developerım",
+    },
+    description: {
+      en: "...who likes to craft solid and scalable frontend products with great user experiences.",
+      tr: "Harika kullanıcı deneyimlerine sahip sağlam ve ölçeklenebilir ön uç ürünleri oluşturmayı seven biriyim.",
+    },
+    github: "https://github.com/",
+    linkedIn: "https://www.linkedin.com/",
+    image: "../../images/profile.png",
+  },
+];
+
+export const skillsData = [
+  {
+    image: "../../images/skills/js.svg",
+    text: "JAVASCRIPT",
+  },
+  {
+    image: "../../images/skills/node.svg",
+    text: "NODE",
+  },
+  {
+    image: "../../images/skills/react.svg",
+    text: "REACT",
+  },
+  {
+    image: "../../images/skills/vscode.png",
+    text: "VS CODE",
+  },
+  {
+    image: "../../images/skills/redux.png",
+    text: "REDUX",
+  },
+  {
+    image: "../../images/skills/figma.svg",
+    text: "FIGMA",
+  },
+];
+
+export const profileData = {
+  firstHeader: { en: "Basic Information", tr: "Temel Bilgiler" },
+  personal: {},
+  lastHeader: { en: "About Me", tr: "Hakkımda" },
+  description:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam! ",
+  image: "../../images/profile.png",
+};
+
+export const projectData = [
+  {
+    header: "Workintech",
+    description: {
+      en: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+      tr: "Kullanıcılarınızın hangi çerezleri kabul edeceğini veya reddedeceğini seçmesine olanak tanıyan basit, özelleştirilebilir, minimal kurulum çerez eklentisi. Bu, vanilya JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, kod ve temalarda her türlü özelleştirmeyi mümkün kılar.",
+    },
+    buttons: ["react", "redux", "vercel"],
+    links: [
+      { en: "View Site", tr: "İncele" },
+      { en: "GitHub", tr: "GitHub" },
+    ],
+    image: "../../images/project/project1.png",
+  },
+  {
+    header: "Journey",
+    description: {
+      en: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+      tr: "Kullanıcılarınızın hangi çerezleri kabul edeceğini veya reddedeceğini seçmesine olanak tanıyan basit, özelleştirilebilir, minimal kurulum çerez eklentisi. Bu, vanilya JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, kod ve temalarda her türlü özelleştirmeyi mümkün kılar.",
+    },
+    buttons: ["react", "redux", "vercel"],
+    links: [
+      { en: "View Site", tr: "İncele" },
+      { en: "GitHub", tr: "GitHub" },
+    ],
+    image: "../../images/project/project2.png",
+  },
+];
+
+export const footerData = {
+  sendme: { en: "Send me a message!", tr: "Bana Ulaşın!" },
+  description: {
+    en: "Got a question or proposal, or just want to say hello? Go ahead.",
+    tr: "Bir sorunuz veya öneriniz mi var, yoksa sadece merhaba mı demek istiyorsunuz? İlerleyin.",
+  },
+  social: [
+    "../../images/socialmedia/twitter.svg",
+    "../../images/socialmedia/codepen.svg",
+    "../../images/socialmedia/at-sign.svg",
+    "../../images/socialmedia/instagram.svg",
+  ],
+};
