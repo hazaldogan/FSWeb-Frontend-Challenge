42e570c79dcd724bc0dd199359f4a40ca7816b65
Hazal Taştan <hazl.dogann@gmail.com>
2023-11-05 00:16:45 +0300

    skills modified

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -2,11 +2,35 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Skills from "./components/Skills";
+import { getUsersParams } from "./utils/endpoints";
+import { useAxios } from "./hooks/useAxios";
+import { useEffect } from "react";
+import { Spinner, Alert } from "reactstrap";
 
 function App() {
+  /* const [getUsers, users, usersLoading, usersError] = useAxios([]);
+  useEffect(() => {
+    getUsers(getUsersParams());
+  }, []);
+
+  const userName = users.map((user) => {
+    return user.firstName;
+  });
+  if (usersLoading) {
+    return <Spinner size="lg" />;
+  }
+
+  if (usersError) {
+    return (
+      <div className="main-container flex items-center justify-center content-center">
+        <Alert color="danger">{usersError}</Alert>
+      </div>
+    );
+  } */
+
   return (
     <div className="App  dark:bg-zinc-900">
-      <Header />
+      <Header /* userName={userName} */ />
       <Skills />
     </div>
   );
---
+++ web-sayfam/src/components/Skills.js
@@ -0,0 +1,18 @@
+import { skillsData } from "../api/data";
+
+function Skills() {
+  return (
+    <div className="skills-area dark:bg-zinc-800">
+      <h1 className="dark:text-lime-200">Skills</h1>
+      <div className="skills-list-area">
+        {skillsData.map((skill, i) => (
+          <div className="skill" key={i}>
+            <img src={skill.image} />
+            <h4>{skill.text}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Skills;
